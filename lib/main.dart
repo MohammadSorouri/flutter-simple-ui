e632b75b3921c0032707cb4f162fa62720bf1450
Mohammad Sorouri <sorouri1380@gmail.com>
2019-06-04 20:41:40 +0430

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -21,9 +21,24 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
+  TabController controller;
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = new TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+
     // TODO: implement build
     return Scaffold(
       body: ListView(
@@ -202,9 +217,118 @@ class _MyHomePageState extends State<MyHomePage> {
                 ],
               )
             ],
-          )
+          ),
+          itemCard('FinnNavian', 'assets/ottoman.jpg', false),
+          itemCard('FinnNavian', 'assets/anotherchair.jpg', true),
+          itemCard('FinnNavian', 'assets/chair.jpg', true)
         ],
+      ),
+      bottomNavigationBar: Material(
+        color: Colors.white,
+        child: TabBar(
+          controller: controller,
+          indicatorColor: Colors.yellow,
+          tabs: <Widget>[
+            Tab(icon: Icon(Icons.event_seat, color: Colors.yellow)),
+            Tab(icon: Icon(Icons.timer, color: Colors.grey)),
+            Tab(icon: Icon(Icons.shopping_cart, color: Colors.grey)),
+            Tab(icon: Icon(Icons.person_outline, color: Colors.grey))
+          ],
+        ),
       ),
     );
   }
+}
+
+Widget itemCard(String title,String img,bool isLick){
+  return Padding(
+    padding: EdgeInsets.only(left: 15,right: 15,top: 15),
+    child: Container(
+      height: 150,
+      width: double.infinity,
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          SizedBox(width: 10,),
+          Container(
+            width: 140,
+            height: 150,
+            decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage(img))
+            ),
+          ),
+          SizedBox(width: 4,),
+          Column(
+            children: <Widget>[
+              SizedBox(height: 10,),
+              Row(
+                children: <Widget>[
+                  Text(title,style: TextStyle(
+                    fontFamily: "Quicksand",
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                  SizedBox(width: 77.4,),
+                  Material(
+                    elevation: isLick ? 0:2,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      width: 30,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        borderRadius:  BorderRadius.circular(20),
+                        color: isLick ? Colors.grey.withOpacity(.2):Colors.white,
+                      ),
+                      child: Center(
+                        child: isLick?Icon(Icons.favorite_border):Icon(Icons.favorite,color: Colors.red,),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 5,),
+              Container(
+                width: 175,
+                child: Text("Scandinavian small sized double sofa imported full leather / Dale Italia oil wax leather black",textAlign: TextAlign.left,style: TextStyle(fontFamily: "Quicksand",color: Colors.grey,fontSize: 12),),
+              ),
+              SizedBox(height: 5.0),
+              Row(
+                children: <Widget>[
+                  SizedBox(width: 77.4),
+                  Container(
+                    height: 40.0,
+                    width: 50.0,
+                    color: Color(0xffF9C335),
+                    child: Center(
+                      child: Text(
+                        '\$248',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Quicksand',
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 40.0,
+                    width: 100.0,
+                    color: Color(0xffFEDD59),
+                    child: Center(
+                      child: Text(
+                        'Add to cart',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'Quicksand',
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          )
+        ],
+      ),
+    ),
+  );
 }
